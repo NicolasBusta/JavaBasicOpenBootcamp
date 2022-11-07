5b57828b08060471d7681ad64980cc2d7599a635
NicolasBusta <nicolasbustamanteb1@gmail.com>
2022-11-07 18:42:59 -0300

    Curso de Java Básico/open bootcamp

---
+++ README.md
@@ -0,0 +1,14 @@
+##Curso de Java Básico - Open Bootamp.
+
+-Ejercicio resueltos:
+
+1)Ejercicio Sesión 1:
+
+*Crear un proyecto java desde cero.
+*Crear un paquete.
+*Dentro del paquete crear una clase.
+*Dentro de la clase crear un método main
+*Imprimir todos los tipos de datos vistos.
+
+
+*Por: Nicolas Bustamante.
---
+++ src/ejercicioN1/TiposDeDato.java
@@ -0,0 +1,46 @@
+package ejercicioN1;
+
+public class TiposDeDato {
+	
+	public static void main(String[] args) {
+	
+	//declaracion de variables:
+		
+		//numericos enteros:
+		
+		byte numeroByte = 5;
+		short numeroShort = 3;
+		int numeroEntero = 500;
+		long numeroLong = 34;
+		
+		// numericos en punto flotantes:
+		
+		float numeroFlotante = 85.5f;
+		double numeroDouble = 84.22;
+		
+		//boolenaos:
+		
+		boolean estadoBoleano1 = true;
+		boolean estadoBoleano2 = false;
+		
+		//cadena de caracteres:
+		
+		String datoString ="es un daro string";
+		char datoChar = 'f';
+		
+		//imprimir por consola enteros
+		System.out.println(numeroEntero);
+		System.out.println(numeroByte);
+		System.out.println(numeroShort);
+		System.out.println(numeroLong);
+		//imprimir por consola flotantes
+		System.out.println(numeroFlotante);
+		System.out.println(numeroDouble);
+		//imprimir por consola boolenaos
+		System.out.println(estadoBoleano1);
+		System.out.println(estadoBoleano2);
+		//imprimir por consola caracteres
+		System.out.println(datoString);
+		System.out.println(datoChar);
+	}
+}
